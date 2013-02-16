9b18fd75e54ef92c27b899d145ac69ebab538a6e
Avi-ec2-kyp <ubuntu@ip-10-98-171-12.ec2.internal>
2013-02-16 12:15:20 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuoteMe::Application.config.secret_token = '5dab309129c3f2df070d3dfd3f6c828f7a61460a415b033d1f26a74c5a4f7969ef75c03862e5e4e32c928ad94d570b05edf9c20ca1908d1b74f859a264474709'
